5ba9dd2fc5060902b62a8b0828943a20641a25d8
mohamadhoseinnozaridev <mohamadhoseinnozaridev@gmail.com>
2024-01-14 13:44:15 +0330

    information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Navbar_main_HotelComponent } from './components/page_one/header/navbar_main_Hotel/navbar_main_Hotel.component';
 import { Page_mainComponent } from './page/page_main/page_main.component';
+import { InformationComponent } from './components/page_one/header/information/information.component';
 
 @NgModule({
-  declarations: [AppComponent, Navbar_main_HotelComponent, Page_mainComponent],
+  declarations: [
+    AppComponent,
+    Navbar_main_HotelComponent,
+    Page_mainComponent,
+    InformationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
