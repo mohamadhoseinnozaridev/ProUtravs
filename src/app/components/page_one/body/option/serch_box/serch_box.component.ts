6382a75ea2ec66f9a472e72f4516c63808d42855
mohamadhoseinnozaridev <mohamadhoseinnozaridev@gmail.com>
2024-01-21 10:40:30 +0330

    complet

--- src/app/components/page_one/body/option/serch_box/serch_box.component.ts
+++ src/app/components/page_one/body/option/serch_box/serch_box.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-serch_box',
   templateUrl: './serch_box.component.html',
-  styleUrls: ['./serch_box.component.css']
+  styleUrls: ['./serch_box.component.css'],
 })
 export class Serch_boxComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
